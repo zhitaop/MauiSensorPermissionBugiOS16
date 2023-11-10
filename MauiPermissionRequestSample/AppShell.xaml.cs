a49bf45f280461c8c8e90f609dcb4e49f23d2656
Zhitao Pan <zpan@esri.com>
2023-11-10 16:14:40 +1100

    Initial commit

---
+++ MauiPermissionRequestSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiPermissionRequestSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiPermissionRequestSample/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace MauiPermissionRequestSample;
+
+public partial class MainPage : ContentPage
+{
+	int sensorCount, cameraCount, locationCount = 0;    
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnSensorRequested(object sender, EventArgs e)
+	{
+        sensorCount++;
+
+		try
+		{
+			var status = await Permissions.RequestAsync<Permissions.Sensors>();
+			debugText.Text += $"Sensor Permission Request result: {status}, click counter {sensorCount}\n";
+		}
+		catch (Exception ex)
+		{
+			debugText.Text += ex.ToString();
+		}
+	}
+
+    private async void OnCameraRequested(object sender, EventArgs e)
+    {
+        cameraCount++;
+
+        try
+        {
+            var status = await Permissions.RequestAsync<Permissions.Camera>();
+            debugText.Text += $"Camera Permission Request result: {status}, click counter {cameraCount}\n";
+        }
+        catch (Exception ex)
+        {
+            debugText.Text += ex.ToString();
+        }
+    }
+
+    private async void OnLocationRequested(object sender, EventArgs e)
+    {
+        locationCount++;
+
+        try
+        {
+            var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            debugText.Text += $"Location Permission Request result: {status}, click counter {locationCount}\n";
+        }
+        catch (Exception ex)
+        {
+            debugText.Text += ex.ToString();
+        }
+    }
+}
+
